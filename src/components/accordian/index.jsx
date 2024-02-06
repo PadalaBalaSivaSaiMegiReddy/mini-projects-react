2ac725e1287427261809a3d58bad0d16ad157b33
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-06 22:56:05 +0530

    Add state for selected item in Accordian component

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react"
+import data from "./data"
 
 export default function Accordian() {
+    [selected,setSelected]=useState(null)
   return (
-    <div>Accodian</div>
+    <div className="wrapper">
+        <div className="accordian">
+            {
+                data && data.length>0?<>
+                data.map(dataItem =><div>
+                    </div>)
+                </>:<div>No data found!!!</div>
+            }
+        </div>
+    </div>
   )
 }
