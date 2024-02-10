e2b64b0121108704eeb3fd76ca43046a5b0812bb
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-10 22:18:07 +0530

    Refactor RandomColorGenerator component

--- src/components/randon-color-generator/index.jsx
+++ src/components/randon-color-generator/index.jsx
@@ -1,17 +1,46 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function RandomColorGenerator() {
+
+  const[typeOfColorFormat,setTypeOfColorFormat]=useState('hex')
+  const[color,setColor]=useState(`#000000`)
+
+  function randomColorUtility(len){
+    return Math.floor(Math.random()*len)
+  }
+
+
+  const handleHexColorGenerator=()=>{
+    const hex=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+    let hexColor='#'
+
+    for(let i=0;i<6;i++){
+      hexColor+=hex[randomColorUtility(hex.length)]
+    }
+    setColor(hexColor)
+  }
+  const handleRBGColorGenerator=()=>{
+    let r=randomColorUtility(256)
+    let g=randomColorUtility(256)
+    let b=randomColorUtility(256)
+    let rgbColor=`rgb(${r},${g},${b})`
+    setColor(rgbColor)
+
+
+
+  }
   return (
-    <div className="RandomColorGenerator">
+    <div className="RandomColorGenerator" style={{background:color}}>
         <div className="topBtnsBar">
-        <button>Create HEX color</button>       
-             <button>Create RGB color</button>
-             <button>Generate Random color</button>
+        <button onClick={()=>setTypeOfColorFormat('hex')}>Create HEX color</button>       
+             <button onClick={()=>setTypeOfColorFormat('rgb')}>Create RGB color</button>
+             <button onClick={typeOfColorFormat==='hex'?handleHexColorGenerator:handleRBGColorGenerator}>Generate Random color</button>
         </div>
 
         <div className="colorsContainer">
-          <h3>HEX Color</h3>
-          <h1>#9F77B8</h1>
+          <h3>{typeOfColorFormat==="hex"?"HEX Color":"RGB Color"}</h3>
+          <h1>{color}</h1>
         </div>
     </div>
   )
