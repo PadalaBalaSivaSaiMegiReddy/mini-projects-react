e429d0a9b330bef894d8e7ba3880e67236660eff
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-13 18:13:38 +0530

    Add state hooks for images and current slide

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,5 +1,9 @@
+import { useState } from "react"
 import "./styles.css"
 export default function ImageSlider({url,limit}) {
+    const [images,setImages]=useState([]);
+    const [currentSlide,setCurrentSlide]=useState(0);
+    
   return (
     <div className="image-slider">index</div>
   )
