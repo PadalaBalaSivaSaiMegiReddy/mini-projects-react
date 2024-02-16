b3da2729121882397550e05696896b0e15751cfb
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-16 16:50:43 +0530

    Add useEffect and fetchImages function to fetch images from URL

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,8 +1,32 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./styles.css"
 export default function ImageSlider({url,limit}) {
     const [images,setImages]=useState([]);
     const [currentSlide,setCurrentSlide]=useState(0);
+    const [errorMsg,setErrorMsg]=useState(null);
+    const [loading,setLoading]=useState(false)
+
+    const fetchImages=async(getUrl)=>{
+      try{
+        setLoading(true)
+        const response = await fetch(getUrl);
+        const data = await response.json()
+
+        if(data){
+          setImages(data)
+          setLoading(false)
+        }
+
+      }catch(e){
+        setLoading(false)
+        setErrorMsg(e.message)
+      }
+
+    }
+
+    useEffect(()=>{
+      if(url!=='') fetchImages(url)
+    },[url])
     
   return (
     <div className="image-slider">index</div>
