17d04881d1070de6655e51af4ae5a1183ad70b0e
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-17 20:38:32 +0530

    Add async function to fetch products and update state variables

--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -3,9 +3,27 @@ import './styles.css';
 import { useState } from 'react';
 
 export default function LoadMoreData() {
+  async function fetchProducts(){
+    setLoading(true)
+    try{
+      const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${count===0?0:count*10}`)
+      const data = await response.json();
+      setLoading(false);
+      setProducts(data)
+
+    }catch(e){
+      console.log(e)
+      setLoading(false);
+    }
+  }
+
+
   const [loading,setLoading]=useState(false);
   const[products,setProducts]=useState([]);
   const[count,setCount]=useState(0);
+  useEffect(()=>{fetchProducts()},[])
+  console.log(products)
+
   return (
     <div className='loadMoreData'>LoadMoreData</div>
   )
