27343d1a2af553d1ac998c0fdda9c5a5cd948f50
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-18 20:52:36 +0530

    Refactor LoadMoreData component and styles

--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -1,48 +1,66 @@
-import { useEffect } from 'react';
-import './styles.css';
-import { useState } from 'react';
+import { useEffect } from "react";
+import { useState } from "react";
+import "./styles.css";
 
 export default function LoadMoreData() {
-  async function fetchProducts(){
-    setLoading(true)
-    try{
-      const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${count===0?0:count*20}`)
-      const data = await response.json();
-      if(data && data.products&& data.products.length){
-        setProducts(data.products)
-        setLoading(false);
-        console.log(products)
-      }
-      
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [disableButton, setDisableButton] = useState(false);
 
-    }catch(e){
-      console.log(e)
+  async function fetchProducts() {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+
+      const result = await response.json();
+
+      if (result && result.products && result.products.length) {
+        setProducts((prevData) => [...prevData, ...result.products]);
+        setLoading(false);
+      }
+
+      console.log(result);
+    } catch (e) {
+      console.log(e);
       setLoading(false);
     }
   }
 
+  useEffect(() => {
+    fetchProducts();
+  }, [count]);
 
-  const [loading,setLoading]=useState(false);
-  const[products,setProducts]=useState([]);
-  const[count,setCount]=useState(0);
-  useEffect(()=>{fetchProducts()},[])
+  useEffect(() => {
+    if (products && products.length === 100) setDisableButton(true);
+  }, [products]);
 
-  
-return(
-  <div className='loadMoreData'>
-  {loading?
-     (<div>Loading data. Please wait!!!</div>):
-   (
-    <>
-    {
-      products.map((item)=>
-      <div key={item.id} className='product-container'>
-        {products&&products.length?<div className='product'><img src={item.thumbnail} alt={item.title}/>  
-        <p>{item.title}</p></div>:null  }
-          </div>)
-    }
-    </>
-  )
-}</div>)
+  if (loading) {
+    return <div>Loading data ! Please wait.</div>;
+  }
 
+  return (
+    <div className="load-more-container">
+      <div className="product-container">
+        {products && products.length
+          ? products.map((item) => (
+              <div className="product" key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="button-container">
+        <button disabled={disableButton} onClick={() => setCount(count + 1)}>
+          Load More Products
+        </button>
+        {disableButton ? <p>You have reached to 100 products</p> : null}
+      </div>
+    </div>
+  );
 }
