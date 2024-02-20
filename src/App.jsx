d4aa3ee549ddb92f12823d7fb77713aad3041c33
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-20 20:44:54 +0530

    Add TreeView component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ImageSlider from './components/image-slider'
 import LoadMoreData from './components/load-more-data'
 import RandomColorGenerator from './components/randon-color-generator'
 import StarRating from './components/star-rating'
+import TreeView from './components/tree-view'
 
 
 function App() {
@@ -14,7 +15,8 @@ function App() {
       <RandomColorGenerator/> 
       <StarRating noOfStars={10}/>  */}
       {/* <ImageSlider url={'https://picsum.photos/v2/list'} limit={10} page={1}/> */}
-      <LoadMoreData/>
+      {/* <LoadMoreData/> */}
+      <TreeView/>
     </div>
   )
 }
---
+++ src/components/tree-view/index.jsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+export default function TreeView() {
+  return (
+    <div className='tree-view'>
+        Tree View
+    </div>
+  )
+}
