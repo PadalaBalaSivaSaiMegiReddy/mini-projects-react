f013855347ed3931d2c81d0152aa86f6a5aae308
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-22 18:54:49 +0530

    Add useState hook and display children count in menu item

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import MenuList from "./menu-list";
 
 export default function MenuItem({item}) {
+  const [displyCurrentChildren,setDisplyCurrentChildren]=useState({})
   return (
     <div className='menu-item-container'>
         <li>
+          <div>
             <p>{item.label}</p>
+            {item && item.children && item.children.length?<span>+</span>:null}
+
+          </div>
             {
                 item&&item.children &&item.children.length>0?
                 <MenuList list={item.children}/>
