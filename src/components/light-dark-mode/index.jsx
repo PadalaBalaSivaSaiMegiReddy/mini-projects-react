bf865cfe651cd0088e6e946dcec73fe6f9f727a8
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-02-29 21:57:50 +0530

    Add light-dark mode functionality to LightDarkMode component

--- src/components/light-dark-mode/index.jsx
+++ src/components/light-dark-mode/index.jsx
@@ -1,7 +1,19 @@
 import './styles.css'
+import useLocalStorage from "./useLocalStorage"
 
 export default function LightDarkMode() {
+  const [theme,setTheme]=useLocalStorage('theme','dark')
+  const handleToggleTheme=()=>{
+    setTheme(theme==='light'?'dark':'light')
+  }
+  console.log(theme)
   return (
-    <div>LightDarkMode</div>
+    <div className='light-dark-mode' data-theme={theme}>
+      <div className='container'>
+        <p>Hello World !</p>
+        <button onClick={handleToggleTheme}>Change Theme</button>
+      </div>
+      
+    </div>
   )
 }
