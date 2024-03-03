bf681c92e5dde6a1110151c3d907d7b0d2261350
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-03 18:04:55 +0530

    Refactor code: Rename QrCodeGenerator to QRCodeGenerator and add margin-top to data-container

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import ScrollIndicator from './components/scroll-indicator'
 import StarRating from './components/star-rating'
 import TreeView from './components/tree-view'
 import menus from './components/tree-view/data'
-import QrCodeGenerator from './qr-code-generator'
+import QRCodeGenerator from './components/qr-code-generator'
 
 
 function App() {
@@ -23,6 +23,7 @@ function App() {
       {/* <TreeView menus={menus}/> */}
       {/* <QrCodeGenerator/> */}
       {/* <LightDarkMode/> */}
+      {/* <QRCodeGenerator/> */}
       <ScrollIndicator url={"https://dummyjson.com/products?limit=100"}/>
     </div>
   )
