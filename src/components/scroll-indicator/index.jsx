a8e280e94a58022f0dfa26c2282da898cabfe38d
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-03 17:41:39 +0530

    Refactor scroll-indicator component and styles

--- src/components/scroll-indicator/index.jsx
+++ src/components/scroll-indicator/index.jsx
@@ -1,41 +1,83 @@
-import { useEffect, useState } from 'react';
-import './styles.css';
+import { useEffect, useState } from "react";
+import "./styles.css";
 
-export default function ScrollIndicator({url}) {
-    const [data,setData]=useState([]);
-    const [loading,setLoading]=useState(false);
-    const [errorMsg,setErrorMsg]=useState('');
-    const [scrollPercentage,setScrollPercentage]=useState(0)
+export default function ScrollIndicator({ url }) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
-    async function getData(getUrl){
-        setLoading(true)
-        try {
-        const res=await fetch(getUrl);
-        const resData=await res.json();
-        if(resData&&resData.products&&resData.products.length>0){
-
-            setData(resData.products)
-            setLoading(false)
-        }
-            
-        } catch (error) {
-            setErrorMsg(error.message)
-            setLoading(false)
-        }
-        
+  async function fetchData(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(getUrl);
+      const data = await response.json();
+      if (data && data.products && data.products.length > 0) {
+        setData(data.products);
+        setLoading(false);
+      }
+    } catch (e) {
+      console.log(e);
+      setErrorMessage(e.message);
     }
-    useEffect(()=>{
-        getData(url)
-    },[url])
-    console.log(data)
+  }
+
+  useEffect(() => {
+    fetchData(url);
+  }, [url]);
+
+  function handleScrollPercentage() {
+    console.log(
+      document.body.scrollTop,
+      document.documentElement.scrollTop,
+      document.documentElement.scrollHeight,
+      document.documentElement.clientHeight
+    );
+
+    const howMuchScrolled =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    setScrollPercentage((howMuchScrolled / height) * 100);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollPercentage);
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  console.log(data, scrollPercentage);
+
+  if (errorMessage) {
+    return <div>Error ! {errorMessage}</div>;
+  }
+
+  if (loading) {
+    return <div>Loading data ! Pleaae wait</div>;
+  }
+
   return (
-    <div className='scroll-indicator-page'>
-        <h1>Custom Scroll Indicator</h1>
-        <div className="data-container">
-            {data&&data.length>0?
-            data.map((dataItem)=><p>{dataItem.title}</p>)
-            :null}
+    <div className="page-container">
+      <div className="top-container">
+        <div className="scroll-progress-tracking-container">
+          <div
+            className="current-progress-bar"
+            style={{ width: `${scrollPercentage}%` }}
+          ></div>
         </div>
+      </div>
+      <div className="data-container">
+      <h1>Custom Scroll Indicator</h1>
+        {data && data.length > 0
+          ? data.map((dataItem) => <p>{dataItem.title}</p>)
+          : null}
+      </div>
     </div>
-  )
+  );
 }
