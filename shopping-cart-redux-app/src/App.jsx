267d3cf1aae02c77f2b2ecd25512138815270a7b
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-04-01 17:29:08 +0530

    set up

--- shopping-cart-redux-app/src/App.jsx
+++ shopping-cart-redux-app/src/App.jsx
@@ -1,15 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-import Cart from "./pages/cart/Cart";
-import Home from "./pages/home/Home";
 
 export default function App() {
   return (
-    <div>
-      <h1>Navbar</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </div>
-  );
+    <div><h1>HIIIII</h1>
+    ok</div>
+  )
 }
