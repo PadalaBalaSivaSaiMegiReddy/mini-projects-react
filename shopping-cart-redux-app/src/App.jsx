a2df94410d34067f60a9c259608f53747b33c8c6
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-04-01 17:38:44 +0530

    set up initial

---
+++ shopping-cart-redux-app/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <div className=" min-h-screen p-6 bg-white text-gray-600 text-lg">
+        Hi
+      </div>
+    </>
+  );
+}
+
+export default App;
